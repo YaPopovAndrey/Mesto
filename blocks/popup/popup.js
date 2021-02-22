a04c8a0c8e53ec2065aaa46cb43781bda46503e4
Popov Andrey <popov-1710@yandex.ru>
2021-02-22 16:14:38 +0300

    edited README and js

--- blocks/popup/popup.js
+++ blocks/popup/popup.js
@@ -15,8 +15,8 @@ closePopupButton.addEventListener('click', togglePopup);
 savePopupButton.addEventListener('click', togglePopup);
 
 let formElement = document.querySelector('.popup__content');
-let nameInput = document.querySelector('.popup__name-input');
-let jobInput = document.querySelector('.popup__job-input');
+let nameInput = document.querySelectorAll('.popup__input');
+let jobInput = document.querySelectorAll('.popup__input');
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
@@ -24,8 +24,8 @@ function formSubmitHandler (evt) {
     let profileName =  document.querySelector('.profile__name');
     let profileProfession = document.querySelector('.profile__profession');
 
-    profileName.textContent = nameInput.value;
-    profileProfession.textContent = jobInput.value;
+    profileName.textContent = nameInput[0].value;
+    profileProfession.textContent = jobInput[1].value;
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
