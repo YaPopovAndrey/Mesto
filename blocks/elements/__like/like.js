bf1302e3cab27e5eb54cf8abd5603fb4694ed9a6
Popov Andrey <popov-1710@yandex.ru>
2021-02-22 18:05:38 +0300

    исправления в БЭМ

--- blocks/elements/__like/like.js
+++
@@ -1,9 +0,0 @@
-let like = document.querySelector('.elements__like');
-
-function likeActive () {
-    if (like.classList.contains('elements__like_active')) {}
-
-    like.classList.toggle('elements__like_active');
-}
-
-like.addEventListener('click', likeActive);
