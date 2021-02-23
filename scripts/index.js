1518380863b7d1e5bd6c0e52b81d7fdcaa2c2992
Popov Andrey <popov-1710@yandex.ru>
2021-02-23 16:25:25 +0300

    Исправления после первой проверки

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,33 @@
+// Кнопки формы
 let popup = document.querySelector('.popup');
 let showPopupButton = document.querySelector('.profile__edit-button');
 let closePopupButton = document.querySelector('.popup__button-close');
-let savePopupButton = document.querySelector('.popup__button-save');
 
-function togglePopup() {
-    if (popup.classList.contains('popup_is-opened')) {}
+// Поля ввода формы
+let formElement = document.querySelector('.popup__form');
+let Inputs = document.querySelectorAll('.popup__input');
 
-    popup.classList.toggle('popup_is-opened');
+// Поля профиля
+let profileName =  document.querySelector('.profile__name');
+let profileProfession = document.querySelector('.profile__profession');
+
+function openPopup() {
+    popup.classList.add('popup_is-opened');
 
 }
 
-showPopupButton.addEventListener('click', togglePopup);
-closePopupButton.addEventListener('click', togglePopup);
-savePopupButton.addEventListener('click', togglePopup);
-
-let formElement = document.querySelector('.popup__content');
-let nameInput = document.querySelectorAll('.popup__input');
-let jobInput = document.querySelectorAll('.popup__input');
+function closePopup() {
+    popup.classList.remove('popup_is-opened');
+}
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
 
-    let profileName =  document.querySelector('.profile__name');
-    let profileProfession = document.querySelector('.profile__profession');
-
-    profileName.textContent = nameInput[0].value;
-    profileProfession.textContent = jobInput[1].value;
+    profileName.textContent = Inputs[0].value;
+    profileProfession.textContent = Inputs[1].value;
+    closePopup();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
+showPopupButton.addEventListener('click', openPopup);
+closePopupButton.addEventListener('click', closePopup);
