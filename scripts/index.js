21721a8c29fbed7ccd9abc139cf1b983a487ad1f
YaPopovAndrey <popov-1710@ya.ru>
2021-03-06 10:55:39 +0300

    feat:added six cards in box

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,78 @@
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+  const container = document.querySelector('.elements__content');
+
+  function renderList() {
+    const result = initialCards.map(function(item) {
+      return `
+  <li>
+      <div class="elements__element">
+          <img src=${item.link} alt="Домбай" class="elements__image">
+          <h2 class="elements__caption">${item.name}</h2>
+          <button type="button" class="elements__like"></button>
+      </div>
+  </li>
+  `;
+    }).join('');
+
+    container.insertAdjacentHTML('afterbegin', result);
+  }
+
+renderList();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Кнопки формы
 let popup = document.querySelector('.popup');
 let showPopupButton = document.querySelector('.profile__edit-button');
