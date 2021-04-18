2cd6a80b8215bff88874feb247d377bd9c5ebd92
YaPopovAndrey <popov-1710@ya.ru>
2021-04-18 10:58:40 +0300

    добавил класс PopupWithForm

---
+++ scripts/PopupWithForm.js
@@ -0,0 +1,34 @@
+import Popup from './Popup.js';
+
+export default class PopupWithForm extends Popup {
+    constructor(popupSelector, submitForm) {
+        super(popupSelector);
+        this._submitForm = submitForm;
+        this._form = this._popup.querySelector('.popup__form');
+    }
+
+    _getInputValues() {
+        this._formValue = {};
+        this._inputList = this._form.querySelectorAll('.popup__input');
+        this._inputList.forEach((items) => {
+            this._formValue[items.name] = items.value;
+        });
+
+        return this._formValue;
+    }
+
+    _formSubmitHandler(evt) {
+        evt.preventDefault();
+        this._submitForm(this._getInputValues);
+    }
+
+    setEventListeners() {
+        super.setEventListeners();
+        this._form.addEventListener('submit', this._formSubmitHandler);
+    }
+
+    close() {
+        super.close();
+        this._form.reset();
+    }
+}
