f298c47fef6c3e6bb73e6d0f9564cc0738e042fe
YaPopovAndrey <popov-1710@ya.ru>
2021-04-18 16:59:31 +0300

    создал все классы

---
+++ scripts/Popup.js
@@ -0,0 +1,36 @@
+export default class Popup {
+    constructor(popupSelector) {
+        this._popup = document.querySelector(popupSelector);
+        this._closeButton = this._popup.querySelector('.popup__button-close');
+        this._handleEscClose = this._handleEscClose.bind(this);
+    }
+
+    open() {
+        this._popup.classList.add('popup_is-opened');
+        document.addEventListener('keydown', this._handleEscClose);
+    }
+
+    close() {
+        if (this._popup.classList.contains('popup_is-opened')) {
+            this._popup.classList.remove('popup_is-opened');
+        }
+        document.removeEventListener('keydown', this._handleEscClose);
+    }
+
+    _handleEscClose(evt) {
+        if (evt.key === 'Escape') {
+            this.close();
+        }
+    }
+
+    _overlayClose(evt) {
+        if (evt.target === evt.currentTarget) {
+            this.close();
+        }
+    }
+
+    setEventListeners() {
+        this._closeButton.addEventListener('click', () => {this.close();});
+        this._popup.addEventListener('click', (evt) => {this._overlayClose(evt)});
+    }
+}
--- scripts/index.js
+++ scripts/index.js
@@ -21,6 +21,7 @@ import {
 } from '../utils/constants.js';
 import { validationConfig, FormValidator } from './FormValidator.js';
 
+// Открывает попап профиля, заполняет поля имя и профессии, удаляет ошибки
 function openEditProfilePopup() {
   openPopup(popupEditProfile);
   inputName.value = profileName.textContent;
@@ -28,6 +29,7 @@ function openEditProfilePopup() {
   editProfileFormValidator.deleteErrors();
 }
 
+// Передает значиня полей в разметку, закрывает попап профиля
 function editProfileFormSubmitHandler(evt) {
   evt.preventDefault();
 
@@ -36,6 +38,7 @@ function editProfileFormSubmitHandler(evt) {
   closePopup(popupEditProfile);
 }
 
+// Открывает попап новой карточки, удаляет ошибки, деактивирует кнопку, очищает поля ввода
 function openPopupAdd() {
   openPopup(popupAddCard);
 
@@ -48,6 +51,7 @@ function openPopupAdd() {
   inputLink.value = '';
 }
 
+// Создает карточку, добавляет ее в разметку, закрывает попап
 function addFormSubmit(evt) {
   evt.preventDefault();
   const cardElement = createCard({ name: inputTitle.value, link: inputLink.value }, templateCard);
@@ -56,6 +60,7 @@ function addFormSubmit(evt) {
   closePopup(popupAddCard);
 }
 
+// Вешает слушатели на все попапы
 popups.forEach((popup) => {
   popup.addEventListener('click', (evt) => {
     if (evt.target.classList.contains('popup_is-opened')) {
@@ -67,25 +72,30 @@ popups.forEach((popup) => {
   });
 });
 
+// Создает новые элементы
 function createCard(data, template) {
   const card = new Card(data, template);
   const cardElement = card.generateCard();
   return cardElement;
 }
 
+// Слушатели функций
 formElement.addEventListener('submit', editProfileFormSubmitHandler);
 formElementAdd.addEventListener('submit', addFormSubmit);
 popupEditProfileOpenBtn.addEventListener('click', openEditProfilePopup);
 popupAddCardOpenBtn.addEventListener('click', openPopupAdd);
 
+// Отрисовка карточек из массива
 initialCards.forEach((item) => {
   const cardElement = createCard(item, templateCard);
 
   container.append(cardElement);
 });
 
+// Запуск валидации формы профиля
 const editProfileFormValidator = new FormValidator(validationConfig, formElement);
 editProfileFormValidator.enableValidation();
 
+// Запуск валидации формы добавления новой карточки
 const addCardFormValidator = new FormValidator(validationConfig, formElementAdd);
 addCardFormValidator.enableValidation();
