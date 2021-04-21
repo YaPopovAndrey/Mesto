3cf25a861a18b11c3d59210a18be439b0c7e3ca8
YaPopovAndrey <popov-1710@ya.ru>
2021-04-21 18:13:32 +0300

    исправления по ревью номер 1

--- src/page/index.js
+++ src/page/index.js
@@ -1,10 +1,10 @@
-import '../src/page/index.css';
-import Card from './components/Card.js';
-import Section from './components/Section.js';
-import UserInfo from './components/UserInfo.js';
-import PopupWithForm from './components/PopupWithForm.js';
-import PopupWithImage from './components/PopupWithImage.js';
-import { initialCards } from './utils/initial-сards.js';
+import './index.css';
+import Card from '../components/Card.js';
+import Section from '../components/Section.js';
+import UserInfo from '../components/UserInfo.js';
+import PopupWithForm from '../components/PopupWithForm.js';
+import PopupWithImage from '../components/PopupWithImage.js';
+import { initialCards } from '../utils/initial-сards.js';
 import {
   inputName,
   inputJob,
@@ -19,8 +19,8 @@ import {
   container,
   profileName,
   profileProfession
-} from './utils/constants.js';
-import { validationConfig, FormValidator } from './components/FormValidator.js';
+} from '../utils/constants.js';
+import { validationConfig, FormValidator } from '../components/FormValidator.js';
 
 // экземпляры классов
 const userInfo = new UserInfo({ profileName, profileProfession });
